8919ee4ba0af5559b1a84c4f41b0f031bba5eb79
Mateus A <mateusgmaraujo2000@gmail.com>
2024-04-05 17:26:44 -0300

    feat: creating jwt token on successful login

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,68 +1,71 @@
-const UserRepository = require("../repositories/UserRepository");
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const UserRepository = require('../repositories/UserRepository');
 
 class UserController {
-    async show(request, response) {
-        const { email } = request.params;
-        const users = await UserRepository.findEmail(email);
+  async show(request, response) {
+    const { email } = request.params;
+    const users = await UserRepository.findEmail(email);
 
-        if (!users) {
-            return response.status(404).json({ error: "User not found" });
-        }
-
-        response.json(users);
+    if (!users) {
+      return response.status(404).json({ error: 'User not found' });
     }
 
-    async add(request, response) {
-        const { id, email, nome, senha } = request.body;
+    response.json(users);
+  }
 
-        if (!id) {
-            return response.status(400).json({ error: "Id is required" });
-        }
+  async add(request, response) {
+    const {
+      id, email, nome, senha,
+    } = request.body;
 
-        const userExists = await UserRepository.findEmail(email);
-
-        if (userExists.length !== 0) {
-            console.log(email)
-            return response
-                .status(400)
-                .json({ error: "This email already in use" });
-        }
-
-        const create = await UserRepository.createUser({
-            id,
-            email,
-            nome,
-            senha,
-        });
-
-        response.json(create);
+    if (!id) {
+      return response.status(400).json({ error: 'Id is required' });
     }
 
-    async checkValidLogin(response, email) {
-        try{
+    const userExists = await UserRepository.findEmail(email);
 
-            const userMatch = await UserRepository.findEmail(email);
-
-            if(!userMatch)
-                return response.status(401).json({ error: 'Authentication Failed' });
-            
-            const password = await UserRepository.findPassword(email);
-            
-            const passwordMatch = await bcrypt.compare(password, UserRepository.password);
-            
-            if(!passwordMatch)
-                return response.status(401).json({ error: 'Authentication Failed' });
-
-            /* const token = jwt.sign({ UserId: user.id }, 'your-secret-key', { expiresIn: '1h', }); 
-            response.status(200).json({ token }); Att. Mateus */
-
-            response.status(200).json({ success: 'Login success' })
-            
-        }catch(e){
-            response.status(500).json({ error: "Login failed" });
-        }
+    if (userExists.length !== 0) {
+      console.log(email);
+      return response
+        .status(400)
+        .json({ error: 'This email already in use' });
     }
+
+    const create = await UserRepository.createUser({
+      id,
+      email,
+      nome,
+      senha,
+    });
+
+    response.json(create);
+  }
+
+  async checkValidLogin(response, email) {
+    try {
+      const userMatch = await UserRepository.findEmail(email);
+
+      if (!userMatch) return response.status(401).json({ error: 'Authentication Failed' });
+
+      const password = await UserRepository.findPassword(email);
+
+      const passwordMatch = await bcrypt.compare(password, UserRepository.password);
+
+      if (!passwordMatch) return response.status(401).json({ error: 'Authentication Failed' });
+
+
+      // Apesar de nao achar que deve ser usado o userMatch aqui como UserID, coloquei
+      // temporariamente pois nao sabia onde pegar o ID.
+
+      const token = jwt.sign({ UserId: userMatch }, process.env.JWT_SECRET, { expiresIn: 90 });
+      response.status(200).json({ token });
+
+      response.status(200).json({ success: 'Login success' });
+    } catch (e) {
+      response.status(500).json({ error: 'Login failed' });
+    }
+  }
 }
 
 module.exports = new UserController();
--- src/app/repositories/UserRepository.js
+++ src/app/repositories/UserRepository.js
@@ -1,42 +1,43 @@
-const db = require("../../database");
+const db = require('../../database');
 
 class UserRepository {
-    async findEmail(email) {
-        const query = {
-            name: "fetch-email",
-            text: "SELECT * FROM usuario WHERE email = $1",
-            values: [email],
-        };
+  async findEmail(email) {
+    const query = {
+      name: 'fetch-email',
+      text: 'SELECT * FROM usuario WHERE email = $1',
+      values: [email],
+    };
 
-        const rows = await db.query(query);
+    const rows = await db.query(query);
 
-        return rows;
-    }
-    
-    async findPassword(email) {
-        const query = {
-            name: "fetch-email",
-            text: "SELECT senha FROM usuario WHERE email = $1",
-            values: [email],
-        };
+    return rows;
+  }
 
-        const password = await db.query(query);
+  async findPassword(email) {
+    const query = {
+      name: 'fetch-email',
+      text: 'SELECT senha FROM usuario WHERE email = $1',
+      values: [email],
+    };
 
-        return password;
-    }
+    const password = await db.query(query);
 
-    async createUser({ id, email, nome, senha }) {
-        const query = {
-            name: "create-user",
-            text: "INSERT INTO usuario(id, email, nome, senha) \
-            VALUES($1, $2, $3, $4)",
-            values: [id, email, nome, senha],
-        };
+    return password;
+  }
 
-        const rows = await db.query(query);
+  async createUser({
+    id, email, nome, senha,
+  }) {
+    const query = {
+      name: 'create-user',
+      text: 'INSERT INTO usuario(id, email, nome, senha) VALUES($1, $2, $3, $4)',
+      values: [id, email, nome, senha],
+    };
 
-        return rows;
-    }
+    const rows = await db.query(query);
+
+    return rows;
+  }
 }
 
 module.exports = new UserRepository();
