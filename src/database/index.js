2c8faa48d5977f5f2788f4c7b575b4927a1504e1
luizfpbp <luizfpbp@gmail.com>
2024-04-24 15:04:20 -0300

    feat: added postgres auto table creation

--- src/database/index.js
+++ src/database/index.js
@@ -1,18 +1,28 @@
-// const { Pool } = require('pg');
+const { Pool } = require('pg');
+const fs = require('fs');
 
-// const pool = new Pool({
-//     user: 'evaluat-adm',
-//     host: 'localhost',
-//     database: 'postgres',
-//     password: '910f4444bb75187fcbc4ba493281a15b',
-//     port: 5432
-// });
+const pool = new Pool({
+    user: 'evaluat-adm',
+    host: 'localhost',
+    database: 'postgres',
+    password: '910f4444bb75187fcbc4ba493281a15b',
+    port: 5432
+});
 
-// pool.connect();
+pool.connect();
 
-// exports.query = async (query, values) => {
-//   const { rows } = await pool.query(query, values);
-//   return rows;
-// };
+// Create Table from scheme.sql query
+fs.readFile('./src/database/scheme.sql', (err, data) => {
+    if (err) throw err;
+
+    pool.query(data.toString(), (err, res) =>{
+        if (err) throw err;
+    });
+});
+
+exports.query = async (query, values) => {
+   const { rows } = await pool.query(query, values);
+   return rows;
+};
 
 // Foi comentado para nao gerar erro de acesso ao banco de dados em PCs que nao tenham o PostgreSQL
--- src/database/scheme.sql
+++ src/database/scheme.sql
@@ -1,7 +1,8 @@
-create table usuario (
+create table if not exists usuario (
 	usuario_id uuid DEFAULT gen_random_uuid(),
 	email varchar(255) not null,
 	nome varchar(255) not null,
 	senha varchar(255) not null,
+    cargo varchar(255) not null,
     primary key (usuario_id)
 );
