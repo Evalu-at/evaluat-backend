84aaecd6d0f4fa662129998794f946f562c92e22
luizfpbp <luizfpbp@gmail.com>
2024-05-27 09:24:10 -0300

    feat: jwt logic change

--- src/routes.js
+++ src/routes.js
@@ -1,36 +1,46 @@
-const { Router } = require('express');
-const cookieParser = require('cookie-parser');
+const { Router } = require('express')
+const cookieParser = require('cookie-parser')
 
 const ClassController = require('./app/controllers/ClassController')
-const UserController = require('./app/controllers/UserController');
-const Middleware = require('./app/middlewares/Middleware');
+const UserController = require('./app/controllers/UserController')
+const Middleware = require('./app/middlewares/Middleware')
 
-const router = Router();
+const router = Router()
 
-router.use(cookieParser());
+router.use(cookieParser())
 
 router.get('/', (req, res) => {
-  res.send('Hello World from Evalu.at!');
-});
+    res.send('Hello World from Evalu.at!')
+})
 
-router.get('/user/id', UserController.show);
-router.post('/user/add', UserController.add);
-router.post('/user/login', UserController.checkValidLogin);
-router.post('/user/get_feelings', ClassController.getFeelings);
-router.get('/user/verify', UserController.sendEmail); // Envio do email
-router.post('/user/verify', UserController.verifyEmail); // Verificar email
-router.get('/user/logout', Middleware.authorization, UserController.logOut);
+router.get(
+    '/validate-jwt',
+    Middleware.authorization,
+    UserController.validationCheck
+)
 
-router.post('/classroom/add-student', ClassController.addStudent);
-router.post('/classroom/remove-student', ClassController.removeStudent);
-router.post('/classroom/delete', ClassController.deleteClass);
-router.post('/classroom/add', ClassController.addClass);
+router.get('/user/id', UserController.show)
+router.get('/user/logout', Middleware.authorization, UserController.logOut)
+router.post('/user/add', UserController.add)
+router.post('/user/login', UserController.checkValidLogin)
+router.post('/user/get_feelings', ClassController.getFeelings)
+router.post('/user/send-otp', UserController.sendEmail)
+router.post('/user/verify', UserController.verifyEmail)
 
-router.get('/formulario', Middleware.authorization, Middleware.email_verification, UserController.formulario);
+router.post('/classroom/add-student', ClassController.addStudent)
+router.post('/classroom/remove-student', ClassController.removeStudent)
+router.post('/classroom/create-evaluation', ClassController.createEvaluation)
+router.post('/classroom/set-student-evaluation', ClassController.setEvaluation)
+router.post('/classroom/delete', ClassController.deleteClass)
+router.post('/classroom/add', ClassController.addClass)
 
-// Test routes
-router.get('/roleTest', UserController.findRole);
-router.post('/evaluateTest', ClassController.createEvaluation);
-router.post('/classroom/student-evaluation', ClassController.setEvaluation);
+router.get('/find/user-role', UserController.findRole)
 
-module.exports = router;
+router.get(
+    '/formulario',
+    Middleware.authorization,
+    Middleware.email_verification,
+    UserController.formulario
+)
+
+module.exports = router
